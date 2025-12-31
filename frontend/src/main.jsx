3b50b81e99b84fe5762e66bd682ae935a23d7b4c
skmasum98 <skmasum98@gmail.com>
2025-12-31 09:48:13 +0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+
+// Create a client
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
